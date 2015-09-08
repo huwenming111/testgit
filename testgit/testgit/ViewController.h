f195580edffc2184399a7de039241c8f56944670
huwenming111 <295085142@qq.com>
2015-09-08 11:09:01 +0800

    fff

---
+++ testgit/testgit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testgit
+//
+//  Created by WenJieKeJi on 15/9/8.
+//  Copyright (c) 2015年 WenJieKeJi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testgit/testgit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testgit
+//
+//  Created by WenJieKeJi on 15/9/8.
+//  Copyright (c) 2015年 WenJieKeJi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
